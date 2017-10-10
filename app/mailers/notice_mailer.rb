4fce239cf5d4bfb7f9db3fe8a2636c81cbc42fae
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-10-10 08:13:30 +0000

    dive11 課題

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -10,4 +10,11 @@ class NoticeMailer < ApplicationMailer
 
     mail to: "ishikawa@nttpc.co.jp", subject: '【Achieve】ブログが投稿されました'
   end
+  
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: @contact.email,
+         subject: "【Achieve】お問い合わせありがとうございました"
+  end
 end
