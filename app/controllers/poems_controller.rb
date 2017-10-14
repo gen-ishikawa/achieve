5d239ef1e3201455d512b8fbfb58a8753cdab03b
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-10-14 03:49:17 +0000

    通常課題

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,4 +2,13 @@ class PoemsController < ApplicationController
   def index
     @poems = Poem.all
   end
+
+  def show
+    set_poem
+  end
+  
+  private
+    def set_poem
+      @poems = Poem.find(params[:id])
+    end
 end
