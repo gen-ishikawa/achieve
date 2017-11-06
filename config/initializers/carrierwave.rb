31658b16b1a4371ffb9b7dc426e9974e19b6a797
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-06 14:39:33 +0000

    config/initializers/carrierwave.rbの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'achieve-gen'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-gen-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-gen'
     when 'development'
       config.fog_directory = 'achieve-gen-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-gen-development'
