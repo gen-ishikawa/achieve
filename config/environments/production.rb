6053660d86e326ec9aceefb50b2911196ba97d53
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-06 13:28:15 +0000

    config/environments/production.rbをAWS用に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'floating-crag-44053.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'floating-crag-44053.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '54.92.98.70' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
